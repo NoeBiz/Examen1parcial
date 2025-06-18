661b8def28988d083541666f05626ad0ff8b1599
NoeBiz <322030100@upjr.edu.mx>
2025-06-18 10:47:05 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,14 @@
 <?php
-    $host = "db";
-    $user = "root";
-    $clave = "";
-    $bd = "sistema";
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
-    mysqli_set_charset($conexion,"utf8");
+$host = "db";
+$user = "root";
+$clave = "rootpass";
+$bd = "sistema";
+
+$conexion = mysqli_connect($host, $user, $clave, $bd);
+
+if (!$conexion) {
+    die("No se pudo conectar a la base de datos: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conexion, "utf8");
 ?>
